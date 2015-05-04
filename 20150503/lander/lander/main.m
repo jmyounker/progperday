6981a0b62e727d8a9c2fef83e0989406335bdcf6
Jeff Younker <jeff@drinktomi.com>
2015-05-04 02:03:08 +0200

    Initial Commit

---
+++ 20150503/lander/lander/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lander
+//
+//  Created by Jeff Younker on 5/4/15.
+//  Copyright (c) 2015 Jeff Younker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
